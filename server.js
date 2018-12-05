5162e3425b040cc8c036b0fb41b7ab252ca94663
Scott Allan <sallan306@gmail.com>
2018-12-05 10:20:27 -0600

    changed settings for heroku compatability

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 
 const apiRoutes = require("./app/routing/apiRoutes");
