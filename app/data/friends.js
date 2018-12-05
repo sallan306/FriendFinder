24670e8df9118ff4b47f837f15c97b56702b2727
Scott Allan <sallan306@gmail.com>
2018-12-05 03:03:51 -0600

    finished with the basic functionality

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,50 +3,125 @@
 let object = {
     tableData: [
     {
-    "Name": "Kyle",
-    "photo": "409-939-7554",
+    "name": "Thor, God of Thunder",
+    "image": "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/11/03/104819285-thor.530x298.jpg?v=1509730606",
     "scores": [
-        5,
         1,
-        4,
-        4,
-        5,
         1,
-        2,
-        5,
-        4,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
         1   ]
     },{
-    "Name": "Kyle",
-    "photo": "409-939-7554",
+    "name": "Deadpool",
+    "image": "https://amp.businessinsider.com/images/56b3ba68dd0895480b8b4833-750-563.jpg",
+    "scores": [
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2   ]
+    },{
+    "name": "Wolverine",
+    "image": "https://cdn3.movieweb.com/i/article/ti2GElKpcH2ZCKPi3mlPOhXNsj3uTj/798:50/Wolverine-Movie-New-Logan-Hugh-Jackman-Dougray-Scott.jpg",
+    "scores": [
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3   ]
+    },{
+    "name": "Scarlet Witch",
+    "image": "https://static0.srcdn.com/wordpress/wp-content/uploads/2017/01/Scarlet-Witch-Avengers-Disassembled.jpg",
+    "scores": [
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4   ]
+    },{
+    "name": "Thanos",
+    "image": "https://cdn-images-1.medium.com/max/2000/1*bpZ12wWUvrCtzXU0ux0Pjw.jpeg",
     "scores": [
         5,
-        1,
-        4,
-        4,
         5,
-        1,
-        2,
         5,
-        4,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5   ]
+    },{
+    "name": "Iron Man",
+    "image": "https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/10/18/Pictures/_5fb51944-d2ee-11e8-841e-211dfd3178e1.jpg",
+    "scores": [
+        1,
+        3,
+        1,
+        3,
+        3,
+        1,
+        3,
+        3,
+        1,
         1   ]
     },{
-    "Name": "Kyle",
-    "photo": "409-939-7554",
+    "name": "Magneto",
+    "image": "https://static0.srcdn.com/wordpress/wp-content/uploads/xmen-leader-magneto.jpg",
     "scores": [
-        5,
+        2,
+        3,
+        2,
+        3,
+        2,
+        3,
+        2,
+        3,
+        2,
+        3   ]
+    },{
+    "name": "Spider Man",
+    "image": "https://apollo2.dl.playstation.net/cdn/UP9000/CUSA02299_00/FREE_CONTENTlfrM9XqFMPLKmUpQpxJS/PREVIEW_SCREENSHOT3_163995.jpg",
+    "scores": [
         1,
-        4,
+        2,
+        3,
         4,
         5,
         1,
         2,
-        5,
+        3,
         4,
-        1   ]
-    }],
+        5   ]
+    }
+
+
+
+],
+    
     add: function(data){
-        console.log(data);
+        // console.log(data);
         console.log("You have submitted your profile info!");
         object.tableData.push(data);
     }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,37 +7,43 @@
 const express = require('express');
 const router = express.Router();
 
-const {add,tableData} = require('../data/friends.js');
+const {tableData,add} = require('../data/friends.js');
+var userData = [];
 
 
-router.get("/tables", function(req, res){
-    console.log(tableData);
+
+router.get("/reserve", function(req, res){
+    console.log("sending the server data somewhere")
     res.send(tableData);
 });
 
-router.get("/waitlist", function(req, res){
-    console.log("Waitlist API route called.");
-    let waitList = [];
-    for (let i = 5; i < tableData.length; i++){
-        waitList.push(tableData[i]);
-    }
-    res.send(waitList);
-});
-      
-router.get("/reserve", function(req, res){
-    res.send("Get: API/Reserve");
-});
-
-router.post('/clear', function(req, res){
-    tableData.length = 0;
-    res.send(); 
-})
 
 router.post('/reserve', function(req, res){
-    add(req.body);
-    console.log(tableData);
+    userData = req
+    console.log("user's data is stored on the server")
+    // console.log(userData);
+    console.log(userData)
+    console.log(tableData)
     res.send("Post: API/Reserve");
-
+    
 });
 
-module.exports = (router);
+// router.post('/clear', function(req, res){
+    //     tableData.length = 0;
+    //     res.send(); 
+    // })
+
+// router.get("/tables", function(req, res){
+//     console.log(tableData);
+//     res.send(tableData);
+// });
+
+// router.get("/waitlist", function(req, res){
+//     console.log("Waitlist API route called.");
+//     let waitList = [];
+//     for (let i = 5; i < tableData.length; i++){
+//         waitList.push(tableData[i]);
+//     }
+//     res.send(waitList);
+// });
+    module.exports = (router);
